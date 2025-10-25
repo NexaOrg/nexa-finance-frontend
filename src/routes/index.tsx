44d3f8188f8529de5e9b5d11d5b9c1a3131b9f54
Marcos Prado <marcosdelimaprado@gmail.com>
2025-10-24 23:55:21 -0300

    feat: Directories Structure

---
+++ src/routes/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Home } from '../screens/Home';
+
+const Stack = createNativeStackNavigator();
+
+export function Routes() {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen 
+        name="Home"
+        component={Home}
+      />
+    </Stack.Navigator>
+  );
+}
